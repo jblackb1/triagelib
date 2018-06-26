fc28483bcc5fc4cd86e4c4f6048925105f4d32a4
Jacob Blackburn <20370375+jblackb1@users.noreply.github.com>
2018-06-26 13:53:23 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+      name='triagelib',
+      version='1.0',
+      author='Jacob Blackburn',
+      license='MIT',
+      url='https://github.com/jblackb1/triagelib'
+      packages=['triagelib'],
+      description='Cofense Triage REST API wrapper',
+      long_description=open('README.md').read(),
+      zip_safe=True,
+      packages=find_packages()
+      )
